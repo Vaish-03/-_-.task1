66fb67605b854997aaf1f51f7cf83ec30ea61b00
Vaish-03 <115860627+Vaish-03@users.noreply.github.com>
2023-05-31 09:18:50 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,27 +1,27 @@
-function number(num){
-       var result = document.getElementById("input");
-       result.value += num;
-       
-         }
-       
-         //getresult
-       
-         function result(){
-           var result = document.getElementById("input");
-           result.value = eval(result.value)
-       
-         }
-       
-         //clear result
-       
-         function clearResult(){
-           var result = document.getElementById("input");
-           result.value = "";
-       
-         }
-       function deleteChar(){
-           var number = document.getElementById("input");
-           var remove = number.value;
-           remove = remove.slice(0,-1); //remove last character from a string
-           number.value = remove ;
-       }
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
